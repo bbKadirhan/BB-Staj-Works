13699f3ea524100e171bb4da05f0a39aba7b120e
bbKadirhan <bbkadirhan@gmail.com>
2025-09-17 13:35:40 +0300

    Add files via upload

--- Aylar/Eylul/2Hafta/WebsiteTest/index.js
+++ Aylar/Eylul/2Hafta/WebsiteTest/index.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var text = document.querySelector('.text');
-  var newText = 'playing football.';
-
-  setTimeout(function () {
-    text.innerHTML = "Hello"
-    text.style.animation = 'typing 3s steps(19), cursor .4s step-end infinite alternate-reverse, deleting 2s steps(19) 5s forwards';
-  }, 7000); // Adjust the timing according to your need
+  var text = document.getElementById("text")
+  const words = ["Still in progress...", "This animation is cool!!", "Need more content...", "Typewriter effect :)"];
+  let wordIndex = 0;
+  function typeword() {
+    let word = words[wordIndex];
+    text.textContent = word;
+    text.classList.remove("text");
+    text.style.animation = "none"
+    text.offsetHeight;
+    text.style.animation = "typing 3s steps("+ word.length +"), cursor .4s step-end infinite alternate-reverse, deleting 2s steps("+ word.length +") 5s forwards";
+    text.classList.add("text");
+    setTimeout(() => {
+      wordIndex = (wordIndex + 1) % words.length;
+      typeword()
+    }, 7000);
+    text.classList.add("text")
+  }
+  typeword();
 });
